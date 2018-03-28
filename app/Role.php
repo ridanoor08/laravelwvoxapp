910494caee20d486d8b5a951dae9a303afa431c6
Rida <ridanoor08@yahoo.com>
2018-03-28 16:13:08 +0400

    admin master page padding

---
+++ app/Role.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Role extends Model
+{
+    //
+    protected $primaryKey = "role_id";
+
+    protected $fillable = [
+       'role_id', 'role_name'
+    ];
+
+}
---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.admin')
+
+
+
+@section('content')
+
+    <h1>ADMIN</h1>
+
+@stop
+
+
+@section('footer')
+
+    <h1>Footer</h1>
+
+@stop
